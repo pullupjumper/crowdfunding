52ecbdb88ef821fb74a98bf4d88c4d7e1ca4b5d4
Chris <kendiablo0@gmail.com>
2021-09-13 20:05:08 +0800

    Setup files

---
+++ webpack.config.dev.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {index: './src/js/index.js', },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/pages/index.html',
+      filename:'index.html',
+      chunks: ['index']
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+};
