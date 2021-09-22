c21a65e23661ae786617c4b9460c4362df790bf2
Chris <kendiablo0@gmail.com>
2021-09-22 20:54:02 +0800

    Added l-list--pledge, and updated c-modal.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 import "../scss/index.scss";
 
-console.log("fff");
+const hamburger = document.querySelector('.c-header__hamburger');
+const menu = document.querySelector('.c-dropdown__menu');
+const modalOverlay = document.querySelector('.c-modal__overlay');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('js-header__hamburger-closed');
+  menu.classList.toggle('js-dropdown__menu-active');
+  modalOverlay.classList.toggle('js-modal__overlay-active');
+});
